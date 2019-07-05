28b47719f10c8f740af40edcacf82d3a2e4a3c77
neol <neol@NEHA4>
2019-07-05 17:15:04 +1000

    init from local directory

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './reactjs/app.jsx';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.min.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        port: 9090,
+        open: true,
+        hot: true,
+        publicPath: '/dist/'
+    }
+};
