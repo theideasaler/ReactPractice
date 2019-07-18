d7a094dde0e57d60941420e25274f3269546073c
neol <neol@NEHA4>
2019-07-18 17:04:58 +1000

    update form component

--- src/reactjs/components/formComponent.jsx
+++ src/reactjs/components/formComponent.jsx
@@ -2,3 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import uuid from 'uuid';
 import { addArticle } from '../../redux/actions/index';
+
+const mapDispatchToProps = dispatch => ({
+    addArticle: article => dispatch(addArticle(article))
+});
+
+class ConnectedForm extends Component {
+    constructor(){
+        super();
+        this.state = {
+            title: ""
+        };
+    }
+
+    handleChange = e => this.setState({
+        [e.target.id]: e.target.value
+    });
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { title } = this.state;
+        const id = uuid();
+        this.props.addArticle({ title,id });
+        this.setState({title: ""});
+    };
+}
