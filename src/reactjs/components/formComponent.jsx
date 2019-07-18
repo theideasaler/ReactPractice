44f572c58acde477f0f5e8895eb559d8fd31531a
DESKTOP-1KE2VOK\19545 <theideasaler@gmail.com>
2019-07-18 20:34:28 +1000

    update form

--- src/reactjs/components/formComponent.jsx
+++ src/reactjs/components/formComponent.jsx
@@ -26,4 +26,29 @@ class ConnectedForm extends Component {
         this.props.addArticle({ title,id });
         this.setState({title: ""});
     };
+
+    render(){
+        const { title } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                value={title}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-success btn-lg">
+              SAVE
+            </button>
+            </form>
+        );
+    }
 }
+
+const Form = connect(null, mapDispatchToProps)(ConnectedForm);
+
+export default Form;
