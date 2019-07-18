7268cbf8e510eabe45f1afaa4e1a0f0cae9f004c
neol <neol@NEHA4>
2019-07-18 12:23:48 +1000

    finnish export test

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,17 @@
 import store from './store/index';
 import { addArticle } from './actions/index';
-import tar1 from './testexports/exportsource';
-import tar2 from './testexports/exportsource2';
+// import tar1 from './testexports/exportsource';
+// import tar2 from './testexports/exportsource2';
+// import * as settings from './testexports/expotsource3';
 
 console.log("============*start*============");
-console.log(tar1);
-console.log("tar1: ", Object.getOwnPropertyDescriptors(tar1), "\n tar2: ", Object.getOwnPropertyDescriptors(tar2));
+// settings.var1 = 10;
+// console.log(settings.var1.value, Object.getOwnPropertyDescriptors(settings.var1));
+// setTimeout(() => {
+    //     tar1 = {}; // throw an uncaught error for tar1, which means all the imported APIs are read-only
+    // }, 3000);
+// console.log(tar1);
+// console.log("tar1: ", Object.getOwnPropertyDescriptors(tar1), "\n tar2: ", Object.getOwnPropertyDescriptors(tar2));
 // console.log(store);
 // console.log(store.getState());
 store.subscribe(() => {
---
+++ src/redux/testexports/expotsource3.js
@@ -0,0 +1,9 @@
+export const var1 = {
+    value: 45,
+    get (){
+        this.value = 3;
+    }
+};
+export const var2 = 3;
+
+
